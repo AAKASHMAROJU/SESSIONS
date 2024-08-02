eace4f0a741be3ac3a84513f9d2c2aa564605f01
aakashmaroju <marojuaakash2003@gmail.com>
2024-08-02 07:25:22 +0530

    added flash

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 const express = require("express");
 
 const app = express();
-
 const session = require("express-session");
 
-app.use(session({ secret: "mysupersecretseting" }));
+const flash = require("connect-flash");
+
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(flash());
+
+app.get("/getName", (req, res) => {
+  const { name = "Anonymous" } = req.query;
+  req.session.name = name;
+  req.flash("success", "User Registered Successfully");
+  res.send(`Heyy this is ${name}`);
+});
 
 app.get("/text", (req, res) => {
-  res.send("Test is Successful");
+  res.send(`This text is from ${req.session.name}`);
+  // res.send("Test is Successful");
+});
+
+app.get("/register", (req, res) => {
+  res.render("index.ejs", {
+    name: req.session.name,
+    msg: req.flash("success"),
+  });
+});
+
+app.get("/count", (req, res) => {
+  if (req.session.count) {
+    ++req.session.count;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`The Count you visited is : ${req.session.count} `);
 });
 
 app.listen(3000, () => {
